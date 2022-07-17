60ef74e9ff1eee098fc1506d9f93a60b8f6a26fd
David Benton <30560845+dajabe@users.noreply.github.com>
2022-07-17 13:56:35 +1200

    Add get routes for books

--- server/db/queries/books.js
+++ server/db/queries/books.js
@@ -13,7 +13,21 @@ function getBooksByAuthor(author_id, db = conn) {
   return db('books')
     .join('book_authors', 'book_authors.book_id', 'books.id')
     .join('authors', 'authors.id', 'book_authors.author_id')
-    .where('authors.id', 'is', author_id)
+    .where('authors.id', '=', author_id)
+    .select(
+      'book_id',
+      'series',
+      'title',
+      'is_fiction',
+      'genre',
+      'referral',
+      'books.img_url as book_img',
+      'books.url as book_url',
+      'name as author_name',
+      'authors.img_url as author_img',
+      'authors.url as author_url',
+      'is_read'
+    )
 }
 
 module.exports = {
--- server/routes/books.js
+++ server/routes/books.js
@@ -1,8 +1,24 @@
 const express = require('express')
 const router = express.Router()
 
-router.get('/', (req, res) => {
-  res.send('woo')
+const {
+  getBooks,
+  getBookById,
+  getBooksByAuthor,
+} = require('../db/queries/books')
+
+router.get('/', async (req, res) => {
+  const books = req.query.author
+    ? await getBooksByAuthor(Number(req.query.author))
+    : await getBooks()
+  res.json(books)
 })
+
+router.get('/:id', async (req, res) => {
+  const books = await getBookById(req.params.id)
+  res.json(books)
+})
+
+router.get('/')
 
 module.exports = router
