a3155fd7130012ef94245169b9ffec3bb1edc372
David Benton <30560845+dajabe@users.noreply.github.com>
2022-07-17 22:15:14 +1200

    Add the redux slice for the list of books

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+
 import Header from './Header'
 import Content from './Content'
 import Navigation from './Navigation'
+import { getBookList } from '../redux/bookSlice'
 
 function App() {
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(getBookList())
+  }, [])
+
   return (
     <>
       <Header />
--- client/redux/store.js
+++ client/redux/store.js
@@ -1,5 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit'
+import bookSlice from './bookSlice'
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    bookSlice,
+  },
 })
